75cad06ab5403e0c85f8e98ea1e47b14e81af214
Yurex <yusufekungomi@gmail.com>
2025-09-13 18:54:58 +0100

    installed shadcn ui

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <div>
+      <h1 className='text-red-500 font-black text-bold'>Welcome yusuf</h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam doloribus
+      quas debitis. Optio, nam laudantium iure odit nisi exercitationem, culpa,
+      temporibus accusantium deleniti ad cupiditate! Incidunt, assumenda! Sunt,
+      quia fugit.
+    </div>
+  );
+}
